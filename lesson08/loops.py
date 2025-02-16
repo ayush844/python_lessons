63f663b118055e9b22eb99fdcf443569967e8239
ayush844 <ayush.xyz1625@gmail.com>
2025-02-16 12:31:47 +0530

    learn loops
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

---
+++ lesson08/loops.py
@@ -0,0 +1,159 @@
+value = 1
+
+
+# while value <= 10:
+#     print(value)
+#     if value == 5:
+#         break
+#     value += 1
+
+# # 1
+# # 2
+# # 3
+# # 4
+# # 5
+# # 5    
+
+
+
+# while value <= 10:
+#     value += 1
+#     if value == 5:
+#         continue
+#     print(value)
+# else:
+#     print("Value is now equal to " + str(value))
+
+# # 2
+# # 3
+# # 4
+# # 6
+# # 7
+# # 8
+# # 9
+# # 10
+# # 11
+# # Value is now equal to 11
+
+
+
+
+names = ["Dave", "Sara", "John"]
+
+# for x in names:
+#     print(x)
+
+# # Dave
+# # Sara
+# # John    
+
+
+# for x in "Mississippi":
+#     print(x)
+
+# # M
+# # i
+# # s
+# # s
+# # i
+# # s
+# # s
+# # i
+# # p
+# # p
+# # i
+
+
+# for x in names:
+#     if x == "Sara":
+#         break
+#     print(x)
+
+# #Dave
+
+
+# for x in names:
+#     if x == "Sara":
+#         continue
+#     print(x)
+
+# # Dave
+# # John
+
+
+# for x in range(4):
+#     print(x)
+
+# # 0
+# # 1
+# # 2
+# # 3
+
+
+# for x in range(2, 4):
+#     print(x)
+
+# # 2
+# # 3
+
+
+# for x in range(5, 101, 5):
+#     print(x)
+# else: # >> it runs when for loop ends not when for loop ends with break
+#     print("Glad that\'s over!")
+
+# # 5
+# # 10
+# # 15
+# # 20
+# # 25
+# # 30
+# # 35
+# # 40
+# # 45
+# # 50
+# # 55
+# # 60
+# # 65
+# # 70
+# # 75
+# # 80
+# # 85
+# # 90
+# # 95
+# # 100
+# # Glad that's over!
+
+
+names = ["Dave", "Sara", "John"]
+actions = ["codes", "eats", "sleeps"]
+
+
+# for name in names:
+#     for action in actions:
+#         print(name + " " + action + ".")
+
+# # Dave codes.
+# # Dave eats.
+# # Dave sleeps.
+# # Sara codes.
+# # Sara eats.
+# # Sara sleeps.
+# # John codes.
+# # John eats.
+# # John sleeps.
+
+
+# for action in actions:
+#     for name in names:
+#         print(name + " " + action + ".")
+
+# # Dave codes.
+# # Sara codes.
+# # John codes.
+# # Dave eats.
+# # Sara eats.
+# # John eats.
+# # Dave sleeps.
+# # Sara sleeps.
+# # John sleeps.        
