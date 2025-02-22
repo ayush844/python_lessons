11c96280ef24ecfa84f082286fc090257ed639bd
ayush844 <ayush.xyz1625@gmail.com>
2025-02-22 11:04:48 +0530

    learn closures
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

---
+++ lesson12/closure.py
@@ -0,0 +1,34 @@
+# Closure is a function having access to the scope of its parent
+# function after the parent function has returned.
+
+def parent_function(person, coins):
+    # coins = 3
+
+    def play_game():
+        nonlocal coins
+        coins -= 1
+
+        if coins > 1:
+            print("\n" + person + " has " + str(coins) + " coins left.")
+        elif coins == 1:
+            print("\n" + person + " has " + str(coins) + " coin left.")
+        else:
+            print("\n" + person + " is out of coins.")
+
+    return play_game
+
+
+tommy = parent_function("Tommy", 3)
+jenny = parent_function("Jenny", 5)
+
+tommy()
+tommy()
+
+# Tommy has 2 coins left.
+# Tommy has 1 coin left.
+
+jenny()
+tommy()
+
+# Jenny has 4 coins left.
+# Tommy is out of coins.
